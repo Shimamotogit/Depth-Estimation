cfb87a6632e231e3fa76a1f860c6e4436dafcd76
simamoto <naoshi0308@outlook.jp>
2023-05-30 11:21:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+# yolov7とMiDaSを用いた相対距離推定
+
+## 入力画像
+![input_photo``](https://tk-2025.oops.jp/git/yolov7_and_midas/photo_2.jpg)
+## 内部処理
+![detect](https://tk-2025.oops.jp/git/yolov7_and_midas/resize/photo_2midas.jpg)
+## 出力画像
+![output_photo](https://tk-2025.oops.jp/git/yolov7_and_midas/photo_22.jpg)
+
+## 出力画像
+![output_photo](https://tk-2025.oops.jp/git/yolov7_and_midas/resize/photo_22.jpg)
+## test
+<img alt="hogehoge" src="https://tk-2025.oops.jp/git/yolov7_and_midas/photo_22.jpg" width="50%">
+"# yolov7_and_midas" 
---
+++ midas_run.py
@@ -0,0 +1,106 @@
+"""Compute depth maps for images in the input folder.
+"""
+
+import torch
+import cv2
+
+import numpy as np
+
+# from imutils.video import VideoStream
+from midas.model_loader import default_models, load_model
+
+first_execution = True
+def process(device, model, model_type, image, input_size, target_size, optimize, use_camera):
+    """
+    Run the inference and interpolate.
+
+    Args:
+        device (torch.device): the torch device used
+        model: the model used for inference
+        model_type: the type of the model
+        image: the image fed into the neural network
+        input_size: the size (width, height) of the neural network input (for OpenVINO)
+        target_size: the size (width, height) the neural network output is interpolated to
+        optimize: optimize the model to half-floats on CUDA?
+        use_camera: is the camera used?
+
+    Returns:
+        the prediction
+    """
+    global first_execution
+
+    if "openvino" in model_type:
+        if first_execution or not use_camera:
+            print(f"    Input resized to {input_size[0]}x{input_size[1]} before entering the encoder")
+            first_execution = False
+
+        sample = [np.reshape(image, (1, 3, *input_size))]
+        prediction = model(sample)[model.output(0)][0]
+        prediction = cv2.resize(prediction, dsize=target_size,
+                                interpolation=cv2.INTER_CUBIC)
+    else:
+        sample = torch.from_numpy(image).to(device).unsqueeze(0)
+
+        if optimize and device == torch.device("cuda"):
+            if first_execution:
+                print("  Optimization to half-floats activated. Use with caution, because models like Swin require\n"
+                      "  float precision to work properly and may yield non-finite depth values to some extent for\n"
+                      "  half-floats.")
+            sample = sample.to(memory_format=torch.channels_last)
+            sample = sample.half()
+
+        if first_execution or not use_camera:
+            height, width = sample.shape[2:]
+            print(f"    Input resized to {width}x{height} before entering the encoder")
+            first_execution = False
+
+        prediction = model.forward(sample)
+        prediction = (
+            torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=target_size[::-1],
+                mode="bicubic",
+                align_corners=False,
+            )
+            .squeeze()
+            .cpu()
+            .numpy()
+        )
+
+    return prediction
+
+
+
+def run(input, model_path="./weights/dpt_beit_large_512.pt", model_type="dpt_beit_large_512", optimize=False, side=False, height=None,
+        square=True, grayscale=False):#quare=False, grayscale=False  
+    """Run MonoDepthNN to compute depth maps.
+
+    Args:
+        input_path (str): path to input folder
+        output_path (str): path to output folder
+        model_path (str): path to saved model
+        model_type (str): the model type
+        optimize (bool): optimize the model to half-floats on CUDA?
+        side (bool): RGB and depth side by side in output images?
+        height (int): inference encoder image height
+        square (bool): resize to a square resolution?
+        grayscale (bool): use a grayscale colormap?
+    """
+    print("Initialize")
+
+    # select device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # print("Device: %s" % device)
+
+    model, transform, net_w, net_h = load_model(device, model_path, model_type, optimize, height, square)
+
+
+    print("Start MIDaS processing")
+    
+    image = transform({"image": input})["image"]
+
+    # compute
+    with torch.no_grad():
+        prediction = process(device, model, model_type, image, (net_w, net_h),input.shape[1::-1],
+                                optimize, False)
+    return prediction
