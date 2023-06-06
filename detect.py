906553c0fcc419aed825473590af1a0e19c5c3ce
Shimamotogit <134857097+Shimamotogit@users.noreply.github.com>
2023-06-06 23:07:55 +0900

    first commit

--- detect.py
+++ detect.py
@@ -3,7 +3,7 @@ import yolo_run
 all = [i for i in range(80)]
 
 yolo_run.detect(source="./input_photo/photo_1.jpg", save_path='./save_data',
-                iou_thres=0.45, conf_thres=0.3, classes=[0, 2])#classes=all
+                iou_thres=0.45, conf_thres=0.3, classes=[0, 2])#classes=all 
 
 # PERSON = 0
 # BICYCLE = 1
